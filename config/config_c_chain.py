c85c0de3e5efc11d9b5252be2ed521f23943bd31
automatafh <felipe.henao@testiando.co>
2022-09-14 11:28:50 -0500

    include config

---
+++ config/config_c_chain.py
@@ -0,0 +1,22 @@
+#<editor-fold desc="Transaction Data">   
+from threading import TIMEOUT_MAX
+
+
+DEFAUL_ADDRESS_WITH_FUNDS = "17c2483a699da91af4da72792e137c1547738740011b5435b1f43ea75cb7ddb9"
+CHAIN_ID = 502
+MAX_FEE_PER_GAS = 50000000000
+MAX_PRIORITY_FEE_PER_GAS = 1000000000
+GAS =1000000
+TOWEI_VALUE = 'ether'
+#</editor-fold>
+
+#<editor-fold desc="Data sendcxhain file">
+CONTRACT_ADDRESS = "0xCFB9f3434E00827cD28db2a1e995399B1aD693D9"
+TIMEOUT = 500
+
+
+#request_meta
+REQUEST_TYPE = "xmlrpc"
+NAME = "Cchain"
+
+# #</editor-fold> 
